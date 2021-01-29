4a3c289ecf73b89cd553a256d8fc2d9c4df9b372
Seungwon <jshtmddnjs@naver.com>
2021-01-29 20:57:47 +0900

    extra

--- pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
+++ pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
@@ -1,8 +1,20 @@
 package com.udacity.pricing;
 
+import com.udacity.pricing.domain.price.Price;
+import com.udacity.pricing.domain.price.PriceRepository;
+import com.udacity.pricing.service.PriceException;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
 
 /**
  * Creates a Spring Boot Application to run the Pricing Service.
@@ -14,6 +26,49 @@ public class PricingServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PricingServiceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner initDatabase(PriceRepository repository) {
+        return args -> {
+            for (int i=1; i<= 20; i++) {
+                Price price = PRICES.get(i);
+                repository.save(price);
+            }
+        };
+    }
+
+
+    /**
+     * Holds {ID: Price} pairings (current implementation allows for 20 vehicles)
+     */
+    private static final Map<Long, Price> PRICES = LongStream
+            .range(1, 20)
+            .mapToObj(i -> new Price("USD", randomPrice(), i))
+            .collect(Collectors.toMap(Price::getVehicleId, p -> p));
+
+    /**
+     * If a valid vehicle ID, gets the price of the vehicle from the stored array.
+     * @param vehicleId ID number of the vehicle the price is requested for.
+     * @return price of the requested vehicle
+     * @throws PriceException vehicleID was not found
+     */
+    public static Price getPrice(Long vehicleId) throws PriceException {
+
+        if (!PRICES.containsKey(vehicleId)) {
+            throw new PriceException("Cannot find price for Vehicle " + vehicleId);
+        }
+
+        return PRICES.get(vehicleId);
+    }
+
+    /**
+     * Gets a random price to fill in for a given vehicle ID.
+     * @return random price for a vehicle
+     */
+    private static BigDecimal randomPrice() {
+        return new BigDecimal(ThreadLocalRandom.current().nextDouble(1, 5))
+                .multiply(new BigDecimal(5000d)).setScale(2, RoundingMode.HALF_UP);
     }
 
 }
